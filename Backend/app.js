396cf430a809df1358ad91253a06d01eb9f1a216
Kirito <kiritokunonline21@gmail.com>
2025-08-21 13:19:43 +0530

    basic-comment-functionality-done

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const commentRoutes = require("./routes/commentRoutes");
 
 const app = express();
 
 // Middlewares
 app.use(cors());
 app.use(bodyParser.json());
+
+
+// Routes
+app.use("/api/comments", commentRoutes);
+
 
 // Test route
 app.get("/", (req, res) => {
